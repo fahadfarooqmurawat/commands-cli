a375929fa1208bb855bab5516fb35294913d427d
fahadfarooqmurawat <fahadfarooqmurawat@gmail.com>
2024-12-22 20:57:34 +0500

    fix color print issue

--- src/command.rs
+++ src/command.rs
@@ -1,31 +1,46 @@
+use atty;
 use core::fmt;
 use serde::Deserialize;
-
-const BLUE: &str = "\x1b[34m";
-const GREEN: &str = "\x1b[32m";
-const WHITE: &str = "\x1b[0m";
+use termcolor::{Color, ColorChoice, StandardStream, WriteColor};
 
 #[derive(Deserialize, Debug)]
 pub struct Command {
-    command: String,
-    description: String,
+    pub command: String,
+    pub description: String,
 }
 
 impl Command {
     pub fn get_command(&self) -> &str {
         &self.command
     }
+
     pub fn get_description(&self) -> &str {
         &self.description
     }
 }
 
 impl fmt::Display for Command {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "{}{}\t{}{}{}",
-            GREEN, self.command, BLUE, self.description, WHITE
-        )
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let color_choice = if atty::is(atty::Stream::Stdout) {
+            ColorChoice::Always
+        } else {
+            ColorChoice::Never
+        };
+
+        let mut stdout = StandardStream::stdout(color_choice);
+
+        stdout
+            .set_color(termcolor::ColorSpec::new().set_fg(Some(Color::Green)))
+            .map_err(|_| fmt::Error)?;
+        write!(f, "{}\t", self.command)?;
+
+        stdout
+            .set_color(termcolor::ColorSpec::new().set_fg(Some(Color::Blue)))
+            .map_err(|_| fmt::Error)?;
+        write!(f, "{}", self.description)?;
+
+        stdout.reset().map_err(|_| fmt::Error)?;
+
+        Ok(())
     }
 }
